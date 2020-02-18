d3327baf6c9ec3d6d851fba586264afe889cea85
Florent Gouyon <fgouyon@excilys.com>
2020-02-18 12:09:30 +0100

    Ajout des Loggers, implémentation dans les catchs et insertion des classe JUnit

---
+++ src/test/java/fr/excilys/computer_database/daoTest/CompanyDAOTest.java
@@ -0,0 +1,19 @@
+package fr.excilys.computer_database.daoTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CompanyDAOTest {
+
+	@Test
+	public void testLister() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testListerIntInt() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/fr/excilys/computer_database/daoTest/ComputerDAOTest.java
@@ -0,0 +1,39 @@
+package fr.excilys.computer_database.daoTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ComputerDAOTest {
+
+	@Test
+	public void testAjouter() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testModifier() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSupprimer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLister() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testListerIntInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAfficherInfoComputer() {
+		fail("Not yet implemented");
+	}
+
+}
